b4d910ec6b4b51f4036f861851ee6e0f13791040
gulshan11 <gulshansavanth@gmail.com>
2024-05-15 17:17:06 -0700

    init

---
+++ app/src/main/java/com/knolfo/android/geoquiz/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.knolfo.android.geoquiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.knolfo.android.geoquiz.ui.theme.GeoQuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeoQuizTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Cyan) {
+        Text(text = "Hi, my name is $name!", modifier = modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun GreetingPreview() {
+    GeoQuizTheme {
+        Greeting("Geoquiz")
+    }
+}
